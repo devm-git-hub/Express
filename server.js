72ddf4a3667c88400880101c0cb909d333e15aa6
Dev Mandal <devmandalgps@gmail.com>
2026-06-12 15:14:35 +0530

    Updated Express.js code

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ let port = 9000;
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
+
+
+app.get('/', (req, res) => {
+    console.log('Request recieved');
+    res.send('Hello');
+});
