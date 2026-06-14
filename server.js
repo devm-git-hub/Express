14b2ae1eb8b1e32b7d2e5bdc6237ab7797104bd8
Dev Mandal <devmandalgps@gmail.com>
2026-06-14 21:51:28 +0530

    Path Parameters

--- server.js
+++ server.js
@@ -7,21 +7,26 @@ app.listen(port, () => {
 });
 
 
-// app.get('/', (req, res) => {
-//     console.log('Request recieved');
-//     let code = "<h1>Hello, World!</h1><h2>Express.js</h2>";
-//     res.send(code);
-// });
-
 app.get('/', (req, res) => {
     res.send("This is the Origin Path");
 });
-app.get('/about', (req, res) => {
-    res.send("This is the About Page");
+
+app.get('/:username/:id', (req, res) => {
+    let { username, id} = req.params;
+    // console.log('Request recieved');
+    let code = `<h1>Hello, World!</h1>@${username}<h2>${id}</h2>`;
+    res.send(code);
 });
-app.get('/contact', (req, res) => {
-    res.send("This is the Contact Page");
-});
-app.get('/cources', (req, res) => {
-    res.send("This is the Cources Page");
-});
+
+// app.get('/', (req, res) => {
+//     res.send("This is the Origin Path");
+// });
+// app.get('/about', (req, res) => {
+//     res.send("This is the About Page");
+// });
+// app.get('/contact', (req, res) => {
+//     res.send("This is the Contact Page");
+// });
+// app.get('/cources', (req, res) => {
+//     res.send("This is the Cources Page");
+// });
